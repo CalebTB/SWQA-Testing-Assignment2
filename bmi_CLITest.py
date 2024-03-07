844ec9ac12c4c005d01a621e6d0bbde59866b929
CalebTB <CalebTByers01@gmail.com>
2024-03-07 15:51:32 -0600

    Updated tests

--- bmi_CLITest.py
+++ bmi_CLITest.py
@@ -1,9 +1,7 @@
 import pytest
 from main import calculate_bmi
-from unittest.mock import patch
 
 
-# WORK ON THIS TOMORROW SEE IF WE CAN FIX IT TO INCLUDE MAIN INPUTS
 @pytest.mark.parametrize("user_input, height_feet, height_inches, weight_pounds", [
     ("1", 5.0, 3.0, 150.0),
     ("2", 0, 0, 0),
